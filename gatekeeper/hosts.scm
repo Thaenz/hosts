92bb98a4f986613ae86e6fd6df34ba480f207a69
Continuous Integration <thaenz@pm.me>
2025-03-02 06:16:18 +0000

    update hosts.scm

--- gatekeeper/hosts.scm
+++ gatekeeper/hosts.scm
@@ -6,14 +6,14 @@
 (define-public gatekeeper-hosts
   (package
     (name "gatekeeper-hosts")
-    (version "23-02-2025")
+    (version "02-03-2025")
     (source (origin
       (method url-fetch)
       (uri (string-append
 	"https://github.com/Thaenz/hosts/releases/download/v" version
 	"/hosts.gz"))
       (sha256
-	(base32 "1nv2673d2m8wpm2cc5bmgv4iqqclkd4j6vpwwl5lfq7k5c450ldi"))))
+	(base32 "0240al9lm4nnkn959dlphy1fqr30pmm8hz27dh0pr89qdffgwmnb"))))
     (build-system copy-build-system)
     (home-page "https://github.com/Thaenz/hosts")
     (synopsis "My massive hosts list")
